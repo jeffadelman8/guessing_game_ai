a40cf1d95a8ec05b33e4044ff7fb0425acf6f44d
Jeff Adelman <jeffadelman8@yahoo.com>
2014-03-20 13:40:40 -0700

    Cleaned up unnecessary code

--- guessing_game.js
+++ guessing_game.js
@@ -3,29 +3,23 @@ var guess;
 var Player = {
 	tooLow: [0],
 	tooHigh: [10000],
-	guess: function(){
+	init: function(){
 		guess = parseInt(prompt("Pick a number... 1-10,000. How about " + Math.floor((this.tooLow[0] + this.tooHigh[0]) / 2)));
 		if (guess == beans) {
 			alert("Congratulations! YOU WON in 1 guess!!");
-		} else if (guess<beans){
-			this.tooLow.unshift(guess);
-		} else {
-			this.tooHigh.unshift(guess);
-		};
-	},
-	init: function(){
-		while(guess!==beans){
-			if (guess<beans){
-				this.tooLow.unshift(guess);
-				guess=parseInt(prompt("Too low! How about " + Math.floor((this.tooLow[0] + this.tooHigh[0]) / 2)));
-			} else if (guess>beans){
-				this.tooHigh.unshift(guess);
-				guess=parseInt(prompt("Too high! How about " + Math.floor((this.tooLow[0] + this.tooHigh[0]) / 2)));
-			} 
+		} else{ 
+			while(guess!==beans){
+				if (guess<beans){
+					this.tooLow.unshift(guess);
+					guess=parseInt(prompt("Too low! How about " + Math.floor((this.tooLow[0] + this.tooHigh[0]) / 2)));
+				} else if (guess>beans){
+					this.tooHigh.unshift(guess);
+					guess=parseInt(prompt("Too high! How about " + Math.floor((this.tooLow[0] + this.tooHigh[0]) / 2)));
+				} 
+			};
 		};
 		alert("Congratulations! YOU WON in " + (this.tooLow.length + this.tooHigh.length) + " guesses!!");
 	}
 };
 
-Player.guess();
 Player.init();
